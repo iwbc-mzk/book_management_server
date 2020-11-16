4bf715f36f2c613cf11841e6761bebf3944ddf14
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-11-16 21:25:01 +0900

    Modelの修正

--- book_management/models.py
+++ book_management/models.py
@@ -1,43 +1,53 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=1000)
-    author_id = models.ForeignKey('Author',
-                                  to_field='author_id',
-                                  related_name='author',
+    author = models.ForeignKey('Author',
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True)
+    series = models.ForeignKey('Series',
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True)
+    publisher = models.ForeignKey('Publisher',
                                   on_delete=models.SET_NULL,
                                   null=True,
                                   blank=True)
-    series_id = models.ForeignKey('Series',
-                                  to_field='series_id',
-                                  on_delete=models.SET_NULL,
-                                  null=True,
-                                  blank=True)
-    publisher_id = models.ForeignKey('Publisher',
-                                     to_field='publisher_id',
-                                     on_delete=models.SET_NULL,
-                                     null=True,
-                                     blank=True)
-    label_id = models.ForeignKey('Label',
-                                 on_delete=models.SET_NULL,
-                                 to_field='label_id',
-                                 null=True,
-                                 blank=True)
-    created_by = models.CharField(max_length=100)
+    label = models.ForeignKey('Label',
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              blank=True)
+    medium = models.ForeignKey('Medium',
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True)
+    already_read = models.BooleanField(default=False)
+    created_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='created_by')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=100)
+    updated_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='updated_by')
     updated_at = models.DateTimeField(auto_now=True)
     del_flg = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        now = timezone.now()
+        if not self.created_at:
+            self.created_at = now
+
+        self.updated_at = now
+
+        super(Book, self).save(*args, **kwargs)
+
 
 class Author(models.Model):
-    author_id = models.AutoField(primary_key=True)
     author_name = models.CharField(max_length=1000)
+    how_to_read = models.CharField(max_length=1000)
     created_by = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_by = models.CharField(max_length=100)
@@ -49,13 +59,11 @@ class Author(models.Model):
 
 
 class Series(models.Model):
-    series_id = models.AutoField(primary_key=True)
     series_name = models.CharField(max_length=1000)
-    author_id = models.ForeignKey('Author',
-                                  to_field='author_id',
-                                  on_delete=models.SET_NULL,
-                                  null=True,
-                                  blank=True)
+    author = models.ForeignKey('Author',
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True)
     created_by = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_by = models.CharField(max_length=100)
@@ -67,7 +75,6 @@ class Series(models.Model):
 
 
 class Publisher(models.Model):
-    publisher_id = models.AutoField(primary_key=True)
     publisher_name = models.CharField(max_length=1000)
     created_by = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -80,13 +87,11 @@ class Publisher(models.Model):
 
 
 class Label(models.Model):
-    label_id = models.AutoField(primary_key=True)
     label_name = models.CharField(max_length=1000)
-    publisher_id = models.ForeignKey('Publisher',
-                                     to_field='publisher_id',
-                                     on_delete=models.SET_NULL,
-                                     null=True,
-                                     blank=True)
+    publisher = models.ForeignKey('Publisher',
+                                  on_delete=models.SET_NULL,
+                                  null=True,
+                                  blank=True)
     created_by = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_by = models.CharField(max_length=100)
@@ -95,3 +100,12 @@ class Label(models.Model):
 
     def __str__(self):
         return self.label_name
+
+
+class Medium(models.Model):
+    medium_name = models.CharField(max_length=100)
+    created_by = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+    del_flg = models.BooleanField(default=False)
