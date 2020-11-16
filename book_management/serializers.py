a85a038166067ae41a2bc1a110bf472bbbeaaf59
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-11-16 21:25:22 +0900

    Serializer, Viewの修正

--- book_management/serializers.py
+++ book_management/serializers.py
@@ -1,29 +1,90 @@
 from collections import OrderedDict
 from rest_framework import serializers
-from .models import Book, Author
+from .models import Book, Author, Series, Publisher, Label, Medium
+import json
 
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['title', 'created_by', 'created_at',
-                  'updated_by', 'updated_at', 'del_flg']
+class MyPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_representation(self, value):
+        if self.pk_field is not None:
+            return self.pk_field.to_representation(value.pk)
+
+        return str(self.get_queryset().get(pk=value.pk))
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
+
     class Meta:
         model = Author
-        fields = ['author_id', 'author_name', 'created_at',
-                  'updated_by', 'updated_at', 'del_flg']
+        fields = '__all__'
 
 
-class BookDetailSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True, source='author_id')
-    series = serializers.StringRelatedField(read_only=True, source='series_id')
-    publisher = serializers.StringRelatedField(read_only=True, source='publisher_id')
-    label = serializers.StringRelatedField(read_only=True, source='label_id')
+class SeriesSerializer(serializers.ModelSerializer):
+    author = MyPrimaryKeyRelatedField(queryset=Author.objects.all())
+
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
+
+    class Meta:
+        model = Series
+        fields = '__all__'
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
+
+    class Meta:
+        model = Publisher
+        fields = '__all__'
+
+
+class LabelSerializer(serializers.ModelSerializer):
+    publisher = MyPrimaryKeyRelatedField(queryset=Publisher.objects.all())
+
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
+
+    class Meta:
+        model = Label
+        fields = '__all__'
+
+
+class MediumSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
+
+    class Meta:
+        model = Medium
+        fields = '__all__'
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = MyPrimaryKeyRelatedField(queryset=Author.objects.all())
+    series = MyPrimaryKeyRelatedField(queryset=Series.objects.all())
+    publisher = MyPrimaryKeyRelatedField(queryset=Publisher.objects.all())
+    label = MyPrimaryKeyRelatedField(queryset=Label.objects.all())
+    medium = MyPrimaryKeyRelatedField(queryset=Medium.objects.all())
+
+    author_url = serializers.HyperlinkedRelatedField(source='author', view_name='author-detail', read_only=True)
+    series_url = serializers.HyperlinkedRelatedField(source='series', view_name='series-detail', read_only=True)
+    publisher_url = serializers.HyperlinkedRelatedField(source='publisher',
+                                                        view_name='publisher-detail',
+                                                        read_only=True)
+    label_url = serializers.HyperlinkedRelatedField(source='label', view_name='label-detail', read_only=True)
+    medium_url = serializers.HyperlinkedRelatedField(source='medium', view_name='medium-detail', read_only=True)
+
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+    updated_by = serializers.ReadOnlyField(source='updated_by.username')
 
     class Meta:
         model = Book
-        fields = ['title', 'author', 'series', 'publisher', 'label',
-                  'created_by', 'created_at', 'updated_by', 'updated_at', 'del_flg']
+        fields = '__all__'
+
+
+
+
+
+
--- book_management/views.py
+++ book_management/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer, BookDetailSerializer
+from rest_framework import generics, viewsets
+from .models import Book, Author, Series, Publisher, Label, Medium
+from .serializers import BookSerializer, AuthorSerializer, SeriesSerializer, PublisherSerializer, LabelSerializer, MediumSerializer
 
 
 # Create your views here.
-class ListBook(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
 
-class DetailBook(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookDetailSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class SeriesViewSet(viewsets.ModelViewSet):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
+
+class LabelViewSet(viewsets.ModelViewSet):
+    queryset = Label.objects.all()
+    serializer_class = LabelSerializer
+
+
+class MediumViewSet(viewsets.ModelViewSet):
+    queryset = Medium.objects.all()
+    serializer_class = MediumSerializer
+
