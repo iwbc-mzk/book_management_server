845cf341392a4cb28815f10ef0895f75ac0acfb7
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-11-21 15:40:51 +0900

    Update views.py
    新規追加・更新時の作成者・更新者を自動取得するように修正

--- book_management/views.py
+++ book_management/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from .models import Book, Author, Series, Publisher, Label, Medium
 from .serializers import BookSerializer, AuthorSerializer, SeriesSerializer, PublisherSerializer, LabelSerializer, MediumSerializer
 
@@ -18,23 +17,43 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
+
 
 class SeriesViewSet(viewsets.ModelViewSet):
     queryset = Series.objects.all()
     serializer_class = SeriesSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
 
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
+
 
 class LabelViewSet(viewsets.ModelViewSet):
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
+
 
 class MediumViewSet(viewsets.ModelViewSet):
     queryset = Medium.objects.all()
     serializer_class = MediumSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user,
+                        updated_by=self.request.user)
 
