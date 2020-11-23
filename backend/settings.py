09a83a05e65ff5e41a2d7d8a422ad43a54236c24
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-11-23 09:48:35 +0900

    Update settings.py
    ページネーションの追加

--- backend/settings.py
+++ backend/settings.py
@@ -109,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 CORS_ORIGIN_WHITELIST = [
