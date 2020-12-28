2251066be4cc3313e41a46a3720d0cb22ecde176
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-12-29 07:04:10 +0900

    create book list display
    書籍一覧画面の開発

--- backend/settings.py
+++ backend/settings.py
@@ -110,8 +110,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 CORS_ORIGIN_WHITELIST = [
